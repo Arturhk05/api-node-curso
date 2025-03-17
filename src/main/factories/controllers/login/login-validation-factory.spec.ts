9c6e2f3dc1715bbdacd4f426ea766be3f55cdd39
arturhk05 <arturhandow@gmail.com>
2025-03-17 18:30:10 -0300

    refactor: separate factories in folders

--- src/main/factories/controllers/login/login-validation-factory.spec.ts
+++ src/main/factories/controllers/login/login-validation-factory.spec.ts
@@ -1,9 +1,9 @@
-import { EmailValidation, RequiredFieldValidation, ValidationComposite } from '../../../presentation/helpers/validators/index'
-import type { EmailValidator } from '../../../presentation/protocols/email-validator'
-import type { Validation } from '../../../presentation/protocols/validation'
+import { EmailValidation, RequiredFieldValidation, ValidationComposite } from '../../../../presentation/helpers/validators/index'
+import type { EmailValidator } from '../../../../presentation/protocols/email-validator'
+import type { Validation } from '../../../../presentation/protocols/validation'
 import { makeLoginValidation } from './login-validation-factory'
 
-jest.mock('../../../presentation/helpers/validators/validation-composite')
+jest.mock('../../../../presentation/helpers/validators/validation-composite')
 
 const makeEmailValidator = (): EmailValidator => {
   class EmailValidatorStub implements EmailValidator {
