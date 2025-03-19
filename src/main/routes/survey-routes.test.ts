4f9b8d3f30d7cf49d25ec3a8670677b83fa9981d
arturhk05 <arturhandow@gmail.com>
2025-03-19 00:08:24 -0300

    test: ensure add survey route returns 403 if no access token is provided

--- src/main/routes/survey-routes.test.ts
+++ src/main/routes/survey-routes.test.ts
@@ -20,7 +20,7 @@ describe('Survey Routes', () => {
   })
 
   describe('POST /surveys', () => {
-    test('Should return 200 on signup', async () => {
+    test('Should return 403 on add survey without access token', async () => {
       // eslint-disable-next-line @typescript-eslint/no-misused-promises
       await request(app)
         .post('/api/surveys')
@@ -33,7 +33,7 @@ describe('Survey Routes', () => {
             answer: 'Answer 2'
           }]
         })
-        .expect(204)
+        .expect(403)
     })
   })
 })
