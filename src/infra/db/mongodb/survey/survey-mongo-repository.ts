dd66e5e0fe7cfe16758b62c02bdae5be27308fb1
arturhk05 <arturhandow@gmail.com>
2025-03-23 13:47:12 -0300

    feat: ensure SurveyMongoRepository load all surveys

--- src/infra/db/mongodb/survey/survey-mongo-repository.ts
+++ src/infra/db/mongodb/survey/survey-mongo-repository.ts
@@ -1,10 +1,23 @@
+import type { LoadSurveysRepository } from '../../../../data/protocols/db/survey/load-surveys-repository'
 import type { AddSurveyModel, AddSurveyRepository } from '../../../../data/usecases/add-survey/db-add-survey-protocols'
+import type { SurveyModel } from '../../../../domain/models/survey'
 import { MongoHelper } from '../helpers/mongo-helper'
 
-export class SurveyMongoRepository implements AddSurveyRepository {
+export class SurveyMongoRepository implements AddSurveyRepository, LoadSurveysRepository {
   async add (data: AddSurveyModel): Promise<void> {
     const surveyCollection = await MongoHelper.getCollection('surveys')
     await surveyCollection.insertOne(data)
     return Promise.resolve()
+  }
+
+  async loadAll (): Promise<SurveyModel[]> {
+    const surveyCollection = await MongoHelper.getCollection('surveys')
+    const surveys = await surveyCollection.find().toArray()
+    return surveys.map(survey => ({
+      id: survey._id.toString(),
+      question: survey.question,
+      answers: survey.answers,
+      date: survey.date
+    }))
   }
 }
